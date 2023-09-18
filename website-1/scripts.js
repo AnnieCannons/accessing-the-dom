9ed913256edd7af0d960d63f99ae86953256ffca
shan wolff <shanwolff182@gmail.com>
2023-09-18 10:56:08 -0700

    finisihed exercises

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,45 +1,32 @@
 // ********************** Exercise 1 ********************** //
 
 // Select and then log the first <p> element on the page.
-
-
-
-
-
-
+let firstP = document.getElementById("paragraph");
+console.log(firstP);
 
 
 // ********************** Exercise 2 ********************** //
 
 // Select and then log all of the list items on the website.
- 
-
-
-
-
-
+let listItems = document.getElementsByTagName("li");
+console.log(listItems); 
 
 
 // ********************** Exercise 3 ********************** //
 
 // Select and then log only the first list item on the website. 
 
-
-
-
-
-
-
+let firstItem = document.getElementById("list").children[0];
+console.log(firstItem);
 
 
 // ********************** Exercise 4 ********************** //
 
-// Give a class name to each of the paragraphs on the website. Select all of the paragraphs with the class name. 
+// Give a class name to each of the paragraphs on the website. 
+// Select all of the paragraphs with the class name. 
 
-
-
-
-
+let paragraphs = document.getElementsByClassName("ps");
+console.log(paragraphs);
 
 
 
@@ -47,3 +34,5 @@
 
 // Select and then log the element with the id of 'heading'
 
+let elementHeading = document.getElementById("heading");
+console.log(elementHeading);
