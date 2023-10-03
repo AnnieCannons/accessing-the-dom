89ddffc2ba68b400b8b6193afd446e9345bdc403
Stay-Hack <staysha.l.hackmann@gmail.com>
2023-10-03 16:03:54 -0700

    complete

--- website-2/scripts.js
+++ website-2/scripts.js
@@ -3,7 +3,8 @@
 
 // Select and then log the elements with the class of 'main-section'
 
-
+let mainElement = document.querySelectorAll(".main-selection");
+console.log(mainElement);
 
 
 
@@ -13,9 +14,9 @@
 // ********************** Exercise 2 ********************** //
 
 // Select and then log the elements with the class of 'important-text'
- 
+let importantText = document.querySelectorAll(".important-text");
 
-
+console.log(importantText)
 
 
 
@@ -24,7 +25,9 @@
 // ********************** Exercise 3 ********************** //
 
 // Select and then log the table data in the website.
+let tableData = document.querySelector(".table td").textContent;
 
+console.log(tableData)
 
 
 
@@ -36,7 +39,8 @@
 // ********************** Exercise 4 ********************** //
 
 // Select the paragraph that is nested inside the div with the class of 'table'
-
+let paraElement = document.querySelector(".table p");
+console.log(paraElement);
 
 
 
