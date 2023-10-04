020c62be770c7dd59ec72b4a6831759210b900cc
TechBasedB <130935290+TechBasedB@users.noreply.github.com>
2023-10-04 12:11:51 -0700

    finalized.

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,6 +1,33 @@
 // ********************** Exercise 1 ********************** //
 
 // Select and then log the first <p> element on the page.
+let paragraphVariable = document.querySelector(".paragraph1");
+// let paragraphElement = document.querySelector('#container .paragraph1');
+console.log(paragraphVariable.textContent);
+// console.log(paragraphVariable.outerHTML);
+
+
+// couple different ways to do this 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -12,7 +39,35 @@
 // ********************** Exercise 2 ********************** //
 
 // Select and then log all of the list items on the website.
- 
+ let listItems = document.getElementsByTagName("li");
+console.log(listItems);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -23,6 +78,31 @@
 // ********************** Exercise 3 ********************** //
 
 // Select and then log only the first list item on the website. 
+let firstListItem = document.getElementById("list").getElementsByTagName("li")[0];
+console.log(firstListItem.textContent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,6 +116,8 @@
 
 // Give a class name to each of the paragraphs on the website. Select all of the paragraphs with the class name. 
 
+let paragraph1Elements = document.querySelectorAll(".paragraph1");
+let paragraph2Elements = document.querySelectorAll(".paragraph2");
 
 
 
@@ -43,7 +125,35 @@
 
 
 
-// ********************** Exercise 4 ********************** //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ********************** Exercise 5 ********************** //
 
 // Select and then log the element with the id of 'heading'
-
+let headerElement = document.getElementById("heading");
+console.log(headerElement.textContent);
