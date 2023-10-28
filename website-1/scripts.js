7472f6d0b76778365845d5878e9b7e4d1bba1d1d
Dorcus Nagadya <dorcusnagadya889@gmail.com>
2023-10-28 16:21:26 -0700

    final

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -2,48 +2,40 @@
 
 // Select and then log the first <p> element on the page.
 
-
-
-
-
-
-
+const firstParagraph = document.querySelector('p');
+console.log(firstParagraph);
 
 // ********************** Exercise 2 ********************** //
 
 // Select and then log all of the list items on the website.
- 
 
 
-
-
-
-
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
 
 // ********************** Exercise 3 ********************** //
 
 // Select and then log only the first list item on the website. 
 
-
-
-
-
-
-
-
+const firstListItem = document.querySelector('li');
+console.log(firstListItem);
 
 // ********************** Exercise 4 ********************** //
 
 // Give a class name to each of the paragraphs on the website. Select all of the paragraphs with the class name. 
 
 
-
-
-
-
+const paragraphs = document.querySelectorAll('p');
+paragraphs.forEach((paragraph) => {
+    paragraph.classList.add('custom-class');
+});
+const paragraphsByClass = document.querySelectorAll('.custom-class');
+console.log(paragraphsByClass);
 
 
 // ********************** Exercise 4 ********************** //
 
 // Select and then log the element with the id of 'heading'
 
+const headingElement = document.getElementById('heading');
+console.log(headingElement);
